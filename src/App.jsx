857a670ee9a7962dd0cbc9459cb037fdb18910d8
Niloufar <nilooooofar97@gmail.com>
2023-11-17 11:30:28 +0100

    add first routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Projects from './pages/projects/Projects'
 function App() {
-  
-
   return (
     <>
-    
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/Projects' element={<Projects/>}></Route>
+      </Routes>
     </>
   )
 }
