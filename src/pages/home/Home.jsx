e454d9e0b6be9c5b4831a8f921895c074ca2ca96
Niloufar <nilooooofar97@gmail.com>
2023-11-19 15:28:54 +0100

    footer added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
+import Footer from "../../components/footer/Footer"
 import Navigation from "../../components/navbar/myNavbar"
 function Home() {
   return (
     <div>
       <Navigation />
+      <Footer/>
     </div>
   )
 }
