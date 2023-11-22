0195e52f316b61df0c48b39a49da36ca1e97161d
Niloufar <nilooooofar97@gmail.com>
2023-11-22 12:30:58 +0100

    navbar styles

--- src/components/navbar/myNavbar.jsx
+++ src/components/navbar/myNavbar.jsx
@@ -3,12 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import './navbar.css'
+import { NavLink } from 'react-router-dom';
+
 function Navigation() {
     const expand = "md";
     return (
         <>
             <Navbar key={expand} expand={expand} className="myNav mb-3">
-              <Container fluid>
+              <Container>
                 <Navbar.Brand href="#">Niloufar</Navbar.Brand>
                 <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
                 <Navbar.Offcanvas
@@ -23,9 +25,9 @@ function Navigation() {
                   </Offcanvas.Header>
                   <Offcanvas.Body>
                     <Nav className="justify-content-end flex-grow-1 pe-3">
-                      <Nav.Link href="/">Home</Nav.Link>
-                      <Nav.Link href="/about">About</Nav.Link>
-                      <Nav.Link href="/projects">Projects</Nav.Link>
+                      <NavLink to='/' className='nav-link'>Home</NavLink>
+                      <NavLink to='/about' className='nav-link'>About</NavLink>
+                      <NavLink to='/projects' className='nav-link'>Projects</NavLink>  
                     </Nav>           
                   </Offcanvas.Body>
                 </Navbar.Offcanvas>
