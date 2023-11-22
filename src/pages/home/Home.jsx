3584ce994aea5f0e9c09208e285454787fa5a4c2
Niloufar <nilooooofar97@gmail.com>
2023-11-22 13:42:18 +0100

    workingon home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
-import { Container, Row , Col} from "react-bootstrap"
-import Footer from "../../components/footer/Footer"
-import Navigation from "../../components/navbar/myNavbar"
-import HeroImg from '../../assets/images/hero.svg'
-import './home.css'
+import { Container, Row, Col } from "react-bootstrap";
+import Footer from "../../components/footer/Footer";
+import Navigation from "../../components/navbar/myNavbar";
+import HeroImg from "../../assets/images/hero.svg";
+import cvPdf from "../../assets/cv/Niloufar.pdf"
+import "./home.css";
 function Home() {
   return (
     <div>
@@ -10,18 +11,28 @@ function Home() {
       <Container className="home-container">
         <Row>
           <Col className="hero-img">
-            <img src={HeroImg} alt="hero-img" className="img-fluid"/>
+            <img src={HeroImg} alt="hero-img" className="img-fluid" />
           </Col>
           <Col className="hero-txt">
-            <h2>Hello, I am</h2>
-            <h1>Niloufar</h1>
-            <h2>Junior React.js Developer</h2>
+            <div className="heroText">
+              <h4>Hi, I am</h4>
+              <h1>Niloufar</h1>
+              <h2>Junior React Developer</h2>
+            </div>
+            <div className="heroButtons">
+              <a href={cvPdf} target="_blank" rel="noopener noreferrer">
+                <button className="hero-btn">Download CV</button>
+              </a>
+              <a href="https://github.com/Niloufar97" target="_blank" rel="noopener noreferrer">
+                <button className="hero-btn">My Github</button>
+              </a>
+            </div>
           </Col>
         </Row>
       </Container>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
