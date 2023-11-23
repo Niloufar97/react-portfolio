a7ea6ae8e9f5daa4fb79756d4af87bbcadbe13cf
Niloufar <nilooooofar97@gmail.com>
2023-11-23 13:11:22 +0100

    margin added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,19 +9,19 @@ function Home() {
     <div>
       <Navigation />
       <Container className="home-container">
-        <Row>
-          <Col className="hero-img" sm={12} lg={6}>
+        <Row className="mt-5">
+          <Col className="hero-img mt-3 mb-3" sm={12} lg={6}>
             <img src={HeroImg} alt="hero-img" className="img-fluid" />
           </Col>
-          <Col className="hero-txt" sm={12} lg={6}>
+          <Col className="hero-txt mt-4" sm={12} lg={6}>
             <div className="heroText">
               <h4>Hi, I am</h4>
               <h1>Niloufar</h1>
               <h2>Junior React Developer</h2>
             </div>
-            <div className="heroButtons">
-              <a href={cvPdf} target="_blank" rel="noopener noreferrer">
-                <button className="hero-btn">Download CV</button>
+            <div className="heroButtons ">
+              <a href={cvPdf} target="_blank" rel="noopener noreferrer" >
+                <button className="hero-btn ml-3">Download CV</button>
               </a>
               <a href="https://github.com/Niloufar97" target="_blank" rel="noopener noreferrer">
                 <button className="hero-btn">My Github</button>
