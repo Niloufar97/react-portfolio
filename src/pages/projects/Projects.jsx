fa815b96c970044f1713b5cbd73e8ce1763a1e43
Niloufar <nilooooofar97@gmail.com>
2023-11-26 09:05:51 +0100

    add project component

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,9 +1,13 @@
+import Project from "../../components/myProjects/Project"
 import Navigation from "../../components/navbar/myNavbar"
 
 function Projects() {
   return (
     <div>
       <Navigation/>
+      <Project/>
+      <Project/>
+      <Project/>
     </div>
   )
 }
