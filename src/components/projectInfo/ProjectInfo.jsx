473898197dcd571b1f6d9752a91ac5f9b1fc2aaa
Niloufar <nilooooofar97@gmail.com>
2023-11-30 23:13:26 +0100

    update projectData and basics of projectinfo page

--- src/components/projectInfo/ProjectInfo.jsx
+++ src/components/projectInfo/ProjectInfo.jsx
@@ -1,16 +1,28 @@
 import { useParams } from "react-router-dom"
 import { ProjectsData } from "../../pages/projects/projectsData"
 import Navigation from "../navbar/myNavbar"
+import { Col , Row} from "react-bootstrap"
 
 function ProjectInfo() {
     const projectId = parseInt(useParams().projectId)
     const projectInformation = ProjectsData.find(project => project.id === projectId )
-    console.log(projectInformation)
-    
   return (
     <div>
       <Navigation/>
-      <h1>{projectInformation.title}</h1>
+      <Row>
+        <Col>
+          <img src={projectInformation.img}></img>
+        </Col>
+        <Col>
+          <h2>{projectInformation.title}</h2>
+          <h4>technologies used in this project:</h4>
+          <p>{projectInformation.tech}</p>
+          <h4>Project Completion:</h4>
+          <p>{projectInformation.date}</p>
+          <h4>Description:</h4>
+          <p>{projectInformation.desc}</p>
+        </Col>
+      </Row>
     </div>
   )
 }
--- src/pages/projects/projectsData.jsx
+++ src/pages/projects/projectsData.jsx
@@ -9,34 +9,47 @@ export const ProjectsData = [
         title: "Todo List",
         img: todoImg,
         tech : "HTML, CSS , Javascript",
-        github: "https://github.com/Niloufar97/to-do-list"
+        github: "https://github.com/Niloufar97/to-do-list",
+        date: "May 2023",
+        desc:"During the JavaScript semester at ReDi-School, I undertook the task of creating a To-Do List application as a homework assignment. This project proved instrumental in enhancing my proficiency with JavaScript, particularly in manipulating the Document Object Model (DOM) and utilizing local storage. Through dynamic updates and event handling, I honed my skills in creating an interactive and user-friendly task management tool. Leveraging local storage not only added persistence to user data but also deepened my understanding of building robust web applications. This project marked a significant milestone in my JavaScript journey, showcasing practical application of acquired knowledge and setting the stage for further exploration in web development."
       },
       {
         id: 2,
         title: "Portfolio",
         img: portfolioImg,
         tech: "HTML, CSS",
-        github: "https://github.com/Niloufar97/Portfolio-HYF"
+        github: "https://github.com/Niloufar97/Portfolio-HYF",
+        date: "Oct 2023",
+        desc:"As part of my coursework during the Hack Your Future semester, I meticulously crafted a personal portfolio using HTML and CSS. This project served as a comprehensive review of my acquired skills in front-end development, showcasing a visually appealing and functional portfolio. While the current design excels in larger screens and mobile devices, I'm eager to enhance its responsiveness further by incorporating the Bootstrap library. This endeavor not only encapsulates my commitment to continual improvement but also underscores my proficiency in crafting engaging and dynamic web experiences."
+
       },
       {
         id: 3,
         title: "Kaospilot-toolbox",
         img: kaospilotImg,
         tech : "HTML, CSS , Javascript",
-        github: "https://github.com/Niloufar97/Kaospilot-toolbox"
+        github: "https://github.com/Niloufar97/Kaospilot-toolbox",
+        date: "Jun 2023",
+        desc: "For the culmination of the JavaScript course at ReDi-School, I collaborated with a dynamic team of 12 individuals to create the Kaospilot-toolbox. This multifaceted project not only deepened our understanding of JavaScript but also provided invaluable lessons in teamwork and collaborative development. From mastering the intricacies of JavaScript to honing effective communication within a group setting, the project served as a nexus of technical and interpersonal growth. Additionally, the experience of utilizing Git and GitHub for version control added a crucial layer of real-world application to our skill set. The Kaospilot-toolbox is a testament to our collective efforts, showcasing not only our JavaScript proficiency but also our ability to navigate the complexities of collaborative development and version control systems."
       },
       {
         id: 4,
         title: "Shopping Card",
         img: shoppingCardImg,
         tech : "HTML, CSS , Javascript",
-        github: "https://github.com/Niloufar97/Restaurant-menu-shopping-cart-"
+        github: "https://github.com/Niloufar97/Restaurant-menu-shopping-cart-",
+        date: "May 2023",
+        desc:"Back at ReDi-School, I took a swing at creating a restaurant menu, but I thought, 'Why not spice things up?' So, I threw in a shopping cart, making it my first dive into JavaScript. Sure, the styling is basic, and don't even get me started on responsiveness (or the lack of it), but this project is my baby. It's where I wrestled with JS, trying to figure out the magic behind a shopping cart. Hours were spent, mistakes were made, but dang, did I learn a ton about JavaScript. It might not be the prettiest thing, but it's a symbol of my coding journey, and I wouldn't trade it for the world."
+
       },
       {
         id: 5,
         title: "Calculator App",
         img: calculatorImg,
         tech : "React.js, ES6",
-        github: "https://github.com/Niloufar97/Calculator-app"
+        github: "https://github.com/Niloufar97/Calculator-app",
+        date: "Aug 2023",
+        desc:"Enter my React journey, and voilà, the Calculator app! While it may not be a polished masterpiece, it's one of the first projects where I delved into the wonders of React.js. Through the ups and downs of wrangling with components and state, this app became my learning canvas. It's not about being the best version; it's about the time I invested to grasp the intricacies of React. Every button click and state change made this project precious in my coding saga."
+
       },
 ]
