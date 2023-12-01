bece92dd9154f7cff79bf45212697b5b25447d39
Niloufar <nilooooofar97@gmail.com>
2023-12-01 13:34:39 +0100

    better styling on project info component

--- src/components/projectInfo/ProjectInfo.jsx
+++ src/components/projectInfo/ProjectInfo.jsx
@@ -1,30 +1,35 @@
-import { useParams } from "react-router-dom"
-import { ProjectsData } from "../../pages/projects/projectsData"
-import Navigation from "../navbar/myNavbar"
-import { Col , Row} from "react-bootstrap"
+import { useParams } from "react-router-dom";
+import { ProjectsData } from "../../pages/projects/projectsData";
+import Navigation from "../navbar/myNavbar";
+import { Col, Container, Row } from "react-bootstrap";
+import "./ProjectInfo.css";
 
 function ProjectInfo() {
-    const projectId = parseInt(useParams().projectId)
-    const projectInformation = ProjectsData.find(project => project.id === projectId )
+  const projectId = parseInt(useParams().projectId);
+  const projectInformation = ProjectsData.find(
+    (project) => project.id === projectId
+  );
   return (
     <div>
-      <Navigation/>
-      <Row>
-        <Col>
-          <img src={projectInformation.img}></img>
-        </Col>
-        <Col>
-          <h2>{projectInformation.title}</h2>
-          <h4>technologies used in this project:</h4>
-          <p>{projectInformation.tech}</p>
-          <h4>Project Completion:</h4>
-          <p>{projectInformation.date}</p>
-          <h4>Description:</h4>
-          <p>{projectInformation.desc}</p>
-        </Col>
-      </Row>
+      <Navigation />
+      <Container>
+        <Row className="py-5 align-items-center">
+          <Col className="proImg-container" md={6}>
+            <img src={projectInformation.img} className="img-fluid"></img>
+          </Col>
+          <Col>
+            <h2>{projectInformation.title}</h2>
+            <h4>technologies used in this project:</h4>
+            <p>{projectInformation.tech}</p>
+            <h4>Project Completion:</h4>
+            <p>{projectInformation.date}</p>
+            <h4>Description:</h4>
+            <p className="description-text">{projectInformation.desc}</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default ProjectInfo
+export default ProjectInfo;
