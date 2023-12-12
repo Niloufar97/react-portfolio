7f5884ee285ec99e773a885bdbfb840d68039ac3
Niloufar <nilooooofar97@gmail.com>
2023-12-12 09:47:03 +0100

    animated skillbar

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,12 +1,36 @@
+import { easeIn } from "framer-motion";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 import "./skill.css";
+
+const skillVariant = {
+  hidden: {
+    width: 0,
+  },
+  visible: (percentage) => ({
+    width: `${percentage}%`,
+    transition: { duration: 1, ease: easeIn },
+  }),
+};
+
 function Skill({ tech, percentage }) {
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+  });
+
   return (
     <div className="skills-container">
-      <h4>{tech}:</h4>
+      <h4>{tech}: {percentage}%</h4>
       <div className="skill-bar">
-        <div className="skill-percentage" style={{ width: `${percentage}%` }}>
-          
-        </div>
+        <motion.div
+          className="skill-percentage"
+          variants={skillVariant}
+          initial="hidden"
+          animate={inView ? "visible" : "hidden"}
+          custom={percentage} 
+          ref={ref}
+        ></motion.div>
       </div>
     </div>
   );
