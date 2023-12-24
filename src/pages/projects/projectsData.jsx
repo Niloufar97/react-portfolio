c41cec658be63579f65a6d82ba92d00d6aa42c86
Niloufar <nilooooofar97@gmail.com>
2023-12-24 21:09:44 +0100

    add my new project to portfolio

--- src/pages/projects/projectsData.jsx
+++ src/pages/projects/projectsData.jsx
@@ -4,6 +4,7 @@ import kaospilotImg from '../../assets/images/project-img/kaospilot-toolbox.png'
 import calculatorImg from '../../assets/images/project-img/calculator5.png'
 import shoppingCardImg from '../../assets/images/project-img/shopping card.png'
 import KraftvaerkImg from '../../assets/images/project-img/flown.png'
+import RecipeAppImg from '../../assets/images/project-img/Recipe.png'
 export const ProjectsData = [   
       {
         id: 1,
@@ -16,6 +17,15 @@ export const ProjectsData = [
       },
       {
         id: 2,
+        title: "Recipe App",
+        img: RecipeAppImg,
+        tech : "HTML, CSS , Javascript",
+        github: "https://github.com/Niloufar97/Recipe-App",
+        date: "Dec 2023",
+        desc: "Join me on a foodie journey with the recipe app I whipped up during the Hack Your Future course. I made it super easy to find yummy recipes by pulling info from JSON files. Each dish comes with a cool pop-up that spills the beans on what you need and how to make it. Want pasta or salad? No prob – just use the filters. And if you're on a hunt for a specific recipe, the search tool's got your back. Best part? You can add your own recipes and be part of the foodie gang. Forget boring cookbooks – this app makes cooking fun and simple! 🍳📱"
+      },
+      {
+        id: 3,
         title: "Kaospilot-toolbox",
         img: kaospilotImg,
         tech : "HTML, CSS , Javascript",
@@ -24,7 +34,7 @@ export const ProjectsData = [
         desc: "For the culmination of the JavaScript course at ReDi-School, I collaborated with a dynamic team of 12 individuals to create the Kaospilot-toolbox. This multifaceted project not only deepened our understanding of JavaScript but also provided invaluable lessons in teamwork and collaborative development. From mastering the intricacies of JavaScript to honing effective communication within a group setting, the project served as a nexus of technical and interpersonal growth. Additionally, the experience of utilizing Git and GitHub for version control added a crucial layer of real-world application to our skill set. The Kaospilot-toolbox is a testament to our collective efforts, showcasing not only our JavaScript proficiency but also our ability to navigate the complexities of collaborative development and version control systems."
       },
       {
-        id: 3,
+        id: 4,
         title: "Portfolio",
         img: portfolioImg,
         tech: "HTML, CSS",
@@ -34,7 +44,7 @@ export const ProjectsData = [
 
       },
       {
-        id: 4,
+        id: 5,
         title: "Todo List",
         img: todoImg,
         tech : "HTML, CSS , Javascript",
@@ -43,7 +53,7 @@ export const ProjectsData = [
         desc:"During the JavaScript semester at ReDi-School, I undertook the task of creating a To-Do List application as a homework assignment. This project proved instrumental in enhancing my proficiency with JavaScript, particularly in manipulating the Document Object Model (DOM) and utilizing local storage. Through dynamic updates and event handling, I honed my skills in creating an interactive and user-friendly task management tool. Leveraging local storage not only added persistence to user data but also deepened my understanding of building robust web applications. This project marked a significant milestone in my JavaScript journey, showcasing practical application of acquired knowledge and setting the stage for further exploration in web development."
       },
       {
-        id: 5,
+        id: 6,
         title: "Shopping Card",
         img: shoppingCardImg,
         tech : "HTML, CSS , Javascript",
@@ -53,7 +63,7 @@ export const ProjectsData = [
 
       },
       {
-        id: 6,
+        id: 7,
         title: "Calculator App",
         img: calculatorImg,
         tech : "React.js, ES6",
