49a511c80f233e5155d268893788c7b8bad0505e
Niloufar <nilooooofar97@gmail.com>
2024-05-14 11:24:47 +0200

    fix footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,16 +2,11 @@ import "./footer.css";
 import linkedinImg from "../../assets/images/social-media/linkedin.svg";
 import instagramImg from "../../assets/images/social-media/instagram.svg";
 import facebookImg from "../../assets/images/social-media/facebook.svg";
-import homeImg from '../../assets/images/social-media/house-fill.svg'
-import phoneImg from '../../assets/images/social-media/telephone-fill.svg'
-import gmailImg from '../../assets/images/social-media/gmail.svg'
-import { Col, Row } from "react-bootstrap";
+import {  Row } from "react-bootstrap";
 function Footer() {
   return (
     <div className="footer-container">
       <Row className="footer-row ">
-        <Col md={6} className="social-media">
-        <h4>Contact Me:</h4>
         <div className="social-media">
           <a
             href="https://www.linkedin.com/in/niloufar-khaefi/"
@@ -29,23 +24,6 @@ function Footer() {
             <img src={facebookImg}></img><span>Niloufar Khaefi</span>
           </a>
         </div>
-        </Col>
-        <Col md={6} className="contact-info">
-          
-            <div className="address">
-              <img src={homeImg}></img>
-              <p>Aarhus, skejbyparken</p>
-            </div>
-            <div className="phone">
-              <img src={phoneImg}></img>
-              <p>+45 61 76 55 96</p>
-            </div>
-            <div className="eamil">
-              <img src={gmailImg}></img>
-              <p>nilooooofar97@gmail.com</p>
-            </div>
-
-        </Col>
       </Row>
     </div>
   );
